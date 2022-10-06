570ab0e4befcc1b6fe40e07cc952afc9544f3043
Harsh-bhatt04 <111497012+Harsh-bhatt04@users.noreply.github.com>
2022-10-06 14:32:18 +0530

    Update stack_using_array.c

--- stack_using_array.c
+++ stack_using_array.c
@@ -11,20 +11,22 @@ void main()
 	int ch;
 	s.Top=-1;
 	printf("____Implementation of Stack using Array____\n\n");
-	printf("**************Menu List**************\n");
+	printf("************** Menu List **************\n");
 	menu: printf(" 1.push operation");
 	printf("\n 2.pop operation");
 	printf("\n 3.display operation");
-	printf("\n 4.exit operation\n");
+	printf("\n 4.peek operation");
+	printf("\n 5.exit operation\n");
 	printf("\nenter your choice :");
 	scanf("%d",&ch);
 	
 	switch(ch)
 	{
 		case 1: 	s.Top=push(s.Top);break;
-		//case 2: 	s.Top=pop(s.Top);break;
+		case 2: 	s.Top=pop(s.Top);break;
 		case 3:		display(s.Top);break;
-		case 4:  	exit(0);break;
+		case 4:		s.Top=peek(s.Top);break;
+		case 5:  	exit(0);break;
 		default :printf("\ninvalid input...\n");	
 	}
 	goto menu;
@@ -68,7 +70,7 @@ int push(int top)
 	int item;
 	if(isfull(top))
 	{
-		printf("\nstack is overflow...");
+		printf("\n!!! stack is overflow...!!!\n\n");
 	}
 	else
 	{
@@ -79,4 +81,23 @@ int push(int top)
 		printf("\n%d is inserted successfully...\n",s.stk[top]);
 		return (top);
 	}
+}
+int pop(int top)
+{
+	if(isempty(top))
+	{
+		printf("\nThe operation is not possible because stack is empty\n");
+	}
+	else
+	{
+		top=top-1;
+		printf("\nelement is poped successfully...\n");
+		return top;
+	}
+}
+int peek(int top)
+{
+	int p=s.stk[top];
+	printf("\n%d is the topmost element of the stack..\n");
+	return p;
 }
